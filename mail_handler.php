a8d143420fbce7398c4cfa1b355b6b03b28d9ba6
Tamara Sagakova <tamarasagakova@gmail.com>
2024-08-28 13:59:55 -0700

    check error

--- mail_handler.php
+++ mail_handler.php
@@ -11,7 +11,7 @@ use PHPMailer\PHPMailer\SMTP;
 
 $mail = new PHPMailer(true);
 
-// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+$mail->SMTPDebug = SMTP::DEBUG_SERVER;
 
 $mail->isSMTP();
 $mail->SMTPAuth = true;
