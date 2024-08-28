a38beb2fbbd4acdcc4f9e6cf87ddadfd7a808a2f
Tamara Sagakova <tamarasagakova@gmail.com>
2024-08-28 13:51:42 -0700

    php form handling

---
+++ mail_handler.php
@@ -0,0 +1,33 @@
+<?php
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$message = $_POST["message"];
+
+require "vendor/autoload.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+$mail = new PHPMailer(true);
+
+// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = "smtp.gmail.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+
+$mail->Username = "tamarasagakova@gmail.com";
+$mail->Password = "jtyjlnalwxnoambr";
+
+$mail->setFrom($email, $name);
+$mail->addAddress("tamarasagakova@gmail.com", "Tamara");
+
+$mail->Body = $message;
+
+$mail->send();
+
+header("Location: sent.html");
